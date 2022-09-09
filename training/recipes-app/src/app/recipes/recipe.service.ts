f36ffa73947d4536ab7aacdbce6c5c2b63dc27c3
Eugenia Villegas <maria.villegas@accenture.com>
2022-09-09 11:28:48 -0300

    Carga de otras tareas. Estaba pusheando desde recetas jaja

--- training/recipes-app/src/app/recipes/recipe.service.ts
+++ training/recipes-app/src/app/recipes/recipe.service.ts
@@ -10,25 +10,28 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
     recipeSelected = new Subject<Recipe>();
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe', 
-            'This is a Test', 
-            'https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Recipe-Card-1a.jpg',
-            [
-                new Ingredient('Cheese', 100),
-                new Ingredient('Strawberries', 20)
-            ]),
-        new Recipe(
-            'What else you need to say?', 
-            'Chocolate cake', 
-            'https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Recipe-Card-1a.jpg',
-            [
-                new Ingredient('Flour', 100),
-                new Ingredient('Chocolate chips', 100)
-            ])
-    ];
 
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe', 
+    //         'This is a Test', 
+    //         'https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Recipe-Card-1a.jpg',
+    //         [
+    //             new Ingredient('Cheese', 100),
+    //             new Ingredient('Strawberries', 20)
+    //         ]),
+    //     new Recipe(
+    //         'What else you need to say?', 
+    //         'Chocolate cake', 
+    //         'https://preppykitchen.com/wp-content/uploads/2022/07/Red-Velvet-Recipe-Card-1a.jpg',
+    //         [
+    //             new Ingredient('Flour', 100),
+    //             new Ingredient('Chocolate chips', 100)
+    //         ])
+    // ];
+
+    private recipes: Recipe[];
+    
     constructor(private slService: ShoppingListService) {}
 
     setRecipes(recipes: Recipe[]) {
--- training/recipes-app/src/app/shared/data-storage.service.ts
+++ training/recipes-app/src/app/shared/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map, tap } from 'rxjs/operators';
 
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipe.service";
@@ -18,10 +19,17 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        this.http.get<Recipe[]>('https://recipe-app-6df85-default-rtdb.firebaseio.com/recipes.json')
-        .subscribe(recipes => {
+        return this.http
+        .get<Recipe[]>('https://recipe-app-6df85-default-rtdb.firebaseio.com/recipes.json')
+        .pipe(map(recipes => {
+            return recipes.map(recipe => {
+                return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []}
+            });
+        }),
+        tap(recipes => {
             this.recipeService.setRecipes(recipes);
-        });
+        })
+        )
     }
     
 
